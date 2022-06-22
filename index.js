607eae1ffa67b415b0b8b3ead5e8b661e32ac46a
aidenm123456 <aidenmawji@gmail.com>
2022-06-21 21:59:56 -0400

    change board size functionality completed, with dynamic rerendering based on input.

--- index.js
+++ index.js
@@ -1,18 +1,62 @@
 
 const squaresContainer = document.querySelector('.squaresContainer');
 
-for (let i=0; i < 256; i++) {
+function createBoard(lw) {
 
-    let newDiv = document.createElement('div');
-    newDiv.classList.add('squares');
-    newDiv.setAttribute('style', 'background:blue;');
+    for (let i=0; i < lw; i++) {
 
-    newDiv.addEventListener("mouseover", function (event) {
-        console.log("hovered over")
-        event.target.style.background = "orange";
-    });
+        let newDiv = document.createElement('div');
+        newDiv.classList.add('squares');
+        newDiv.setAttribute('style', 'background:blue;');
+    
+        newDiv.addEventListener("mouseover", function (event) {
+            console.log("hovered over")
+            event.target.style.background = "orange";
+        });
+    
+        squaresContainer.appendChild(newDiv);
 
-    squaresContainer.appendChild(newDiv);
+}
+
 
     
 }
+
+
+function deleteBoard() {
+
+    let response = prompt('Please enter a number between 1 and 50');
+
+    const megaSquare = document.querySelector('.megaSquare')
+    const squaresContainer = document.querySelector('.squaresContainer')
+    squaresContainer.innerHTML ="";
+    
+    let repeatVar = "repeat(" + response.toString() + ", 1fr)";
+    
+    squaresContainer.style.gridTemplateColumns = repeatVar;
+    createBoard(response ** 2);
+}
+
+function changeBoard() {
+
+    // let response = prompt('Please enter a number between 1 and 50');
+
+    // if (response == "" || response == null || typeof(response) != "number") {
+    //     return
+    // }
+
+    // else {
+    const megaSquare = document.getElementById('megaSquare')
+
+    const squares = document.querySelector('.squaresContainer');
+    squares.innerHTML ="";
+
+    let squaresContainer = document.createElement('div');
+    squaresContainer.classList.add('squaresContainer');
+    megaSquare.appendChild(squaresContainer);
+    // }
+}
+
+
+
+createBoard(144)
